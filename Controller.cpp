80d287e37a8b236d07ba6fff2631653774445372
DavidHiser <davidhiseriii@gmail.com>
2022-04-24 00:34:03 -0500

    added MQTT code (not thread safe, probably doesnt work who knows)

--- Controller.cpp
+++ Controller.cpp
@@ -1,5 +1,68 @@
 #include "Controller.h"
+#include <string>
 
 float Controller::getSpeed() const{
     return speed;
+}
+
+void Controller::initializationSequence() {
+    EthernetInterface eth;
+    eth.set_network("192.168.0.9", "255.255.255.0", "192.168.0.1");
+
+    nsapi_error_t status = eth.connect();
+
+    printf("status: %d\n", status);
+
+    SocketAddress this_address;
+    eth.get_ip_address(&this_address);
+
+    printf("This IP Address: %s\n", this_address.get_ip_address());
+
+
+    SocketAddress computer_address;
+    computer_address.set_ip_address("192.168.0.10");
+    computer_address.set_port(1883);
+
+    
+    TCPSocket socket;
+    MQTTClient client(&socket);
+    int a = socket.open(&eth);
+
+    int b = socket.connect(computer_address);
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+
+    MQTTPacket_connectData connect_data = MQTTPacket_connectData_initializer;
+
+    client.connect(connect_data);
+
+    thread.start(callback([this](){this->listen();}));
+}
+
+void Controller::resetSequence() {
+
+}
+
+void Controller::speedMessageReceived(MQTT::MessageData& md){
+    MQTT::Message &message = md.message;
+    char* incoming_message = (char*)malloc(message.payloadlen + 1);
+    strcpy(incoming_message, (char*)md.message.payload);
+    incoming_message[message.payloadlen] = '\0';
+
+    float data = stof(incoming_message);
+    speed = data;
+    
+    free(incoming_message);
+}
+
+void Controller::listen(){
+    // no clue if this works
+    const char* sub_topic = "test/speed";
+    MQTT::QoS qos = MQTT::QOS0;
+    auto func = [this](MQTT::MessageData& md){this->speedMessageReceived(md);};
+    client.subscribe(sub_topic, qos, (MQTTClient::messageHandler)&func);
+
+    while(true){
+        client.yield(100);
+    }
 }
--- Controller.h
+++ Controller.h
@@ -1,7 +1,19 @@
-class Controller{
+#include "Initializable.h"
+#include <MQTTClientMbedOs.h>
+#include "EthernetInterface.h"
+#include <cstring>
+
+class Controller : public Initializable{
     public:
         Controller();
         float getSpeed() const;
+    protected:
+        void initializationSequence() override;
+        void resetSequence() override;
     private:
+        Thread thread;
+        MQTTClient client;
+        void speedMessageReceived(MQTT::MessageData& md);
+        void listen();
         float speed;
 };
